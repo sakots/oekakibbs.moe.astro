3c96faf59de7fbbd8b26dec0841aec45fdf0ef7c
sakots <sakots8000@gmail.com>
2026-08-23 10:56:49 +0900

    日記帳設置

--- src/data/boards.ts
+++ src/data/boards.ts
@@ -1,25 +1,36 @@
 export type Board = {
-	name: string;
-	description: string;
-	href: string;
+  name: string;
+  description: string;
+  href: string;
+  /** 掲示板のバナー画像URLまたはサムネイル生成URL */
+  banner: string;
 };
 
-// 掲示板を追加する際は、この配列に name / description / href を追加してください。
-// href にはアップロード済み掲示板へのパス、または完全なURLを指定できます。
+// 掲示板を追加する際は、この配列に name / description / href / banner を追加してください。
+// href と banner にはアップロード済み掲示板へのパス、または完全なURLを指定できます。
 export const boards: Board[] = [
-	{
-		name: 'お絵かき掲示板',
-		description: 'イラストを投稿して、気軽に交流できるメインの掲示板です。',
-		href: '/bbs/',
-	},
-	{
-		name: 'らくがき掲示板',
-		description: '思いついたときにさっと描ける、気軽ならくがき用の掲示板です。',
-		href: '/rakugaki/',
-	},
-	{
-		name: 'しぃペインター掲示板',
-		description: 'しぃペインターで描いた作品を投稿できる掲示板です。',
-		href: '/shi-painter/',
-	},
+  {
+    name: 'サンプル&交流',
+    description: 'イラストを投稿して、気軽に交流できるメインの掲示板です。',
+    href: '/bbs/noreita/',
+    banner: 'https://oekakibbs.moe/bbs/noreita/noreita_newimg.php',
+  },
+  {
+    name: 'SQL避難所',
+    description: 'データベース移行時などに利用する掲示板です。',
+    href: '/bbs/eurasia/',
+    banner: 'https://oekakibbs.moe/bbs/eurasia/noreita_newimg.php',
+  },
+  {
+    name: 'サポート',
+    description: 'お絵かき掲示板「noReita」のサポート用掲示板です。',
+    href: '/bbs/support/',
+    banner: 'https://oekakibbs.moe/bbs/support/noreita_newimg.php',
+  },
+  {
+    name: '日記帳',
+    description: 'たまにかく。',
+    href: '/bbs/diary/',
+    banner: 'https://oekakibbs.moe/bbs/diary/noreita_newimg.php',
+  },
 ];
